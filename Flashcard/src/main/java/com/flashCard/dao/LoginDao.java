7802f47f1b230f62a61c4d6247d81f3297fd46d9
LaVonne <lreed130@gmail.com>
2017-11-25 13:23:56 -0600

    test

--- Flashcard/src/main/java/com/flashCard/dao/LoginDao.java
+++ Flashcard/src/main/java/com/flashCard/dao/LoginDao.java
@@ -1,9 +1,15 @@
 package com.flashCard.dao;
 
+import java.sql.Statement;
+
 import com.flashCard.model.User;
 
 public interface LoginDao {
 
 	public void saveLogin(User user);
-	public User validateLogin(User login);
+		
+	public User validateLogin(User login){
+		validateEmail(user);
+		validateUsername(user);
+	}
 }
